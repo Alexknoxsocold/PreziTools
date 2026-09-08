e9193c7029f9fe77a9b477f8752617c878714b19
Alex <alexescomail@gmail.com>
2026-09-08 14:07:18 -0400

    Load shared model signal styles

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import "@/model-signals.css"
 
 const badgeVariants = cva(
   // Whitespace-nowrap: Badges should never wrap.
