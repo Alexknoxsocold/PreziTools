a92dbef81af4132e9a06e8dfa84c641f09acede9
Alex <alexescomail@gmail.com>
2026-09-10 16:39:36 -0400

    Polish NFL/NHL market selector and results hit rates
    Compact NFL/NHL market tabs and surface Best Plays hit-rate context on mobile.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./mobile-nav-fixes.css";
+import "./market-tabs.css";
 import "./wnba-arena-backdrops.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
